d9b04817ae3eb066bbb667749fc87cedb924c322
Dodo-31 <botte6230@gmail.com>
2025-11-20 09:50:28 +0100

    modif

--- 1.2.js
+++ 1.2.js
@@ -3,19 +3,5 @@ let firstName = "Gérard";
 let city = "Paris";
 
 console.log(
-  "Vous vous appelez " +
-    firstName +
-    " " +
-    name +
-    " et vous habitez à " +
-    city +
-    "."
-);
-
-let name = "Lambert";
-let firstName = "Gérard";
-let city = "Paris";
-
-console.log(
   `Vous vous appelez ${firstName} ${name} et vous habitez à ${city}.`
 );
