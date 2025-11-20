3b8aaeecbedd09d4c591e0048c42b93a0082ef3c
Dodo-31 <botte6230@gmail.com>
2025-11-20 09:48:49 +0100

    1.2.js

---
+++ 1.2.js
@@ -0,0 +1,21 @@
+let name = "Lambert";
+let firstName = "Gérard";
+let city = "Paris";
+
+console.log(
+  "Vous vous appelez " +
+    firstName +
+    " " +
+    name +
+    " et vous habitez à " +
+    city +
+    "."
+);
+
+let name = "Lambert";
+let firstName = "Gérard";
+let city = "Paris";
+
+console.log(
+  `Vous vous appelez ${firstName} ${name} et vous habitez à ${city}.`
+);
