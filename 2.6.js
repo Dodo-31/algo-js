ed0a3a868f14b8256d63c363af5dbfa2f68a19aa
Dodo-31 <botte6230@gmail.com>
2025-11-20 21:35:29 +0100

    modif

--- 2.6.js
+++ 2.6.js
@@ -9,16 +9,10 @@ console.log(x);
 f;
 var x = 5;
 console.log(x);
-dd;
-var x = 5;
-console.log(x);
-f;
-var x = 5;
 console.log(x);
 dd;
 var x = 5;
 console.log(x);
 f;
 var x = 5;
-console.log(x);
-dd;
+console.log(x
--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,19 @@
 // eslint.config.js
 module.exports = [
   {
-    files: ['**/*.js'],
+    files: ["**/*.js"],
 
     languageOptions: {
-      ecmaVersion: 2020, // compatible Node
-      sourceType: 'script', // important : pour require()
+      ecmaVersion: 2020,   // compatible Node
+      sourceType: "script" // important pour require()
     },
 
     rules: {
-      'no-var': 'error', // interdit var
-      semi: ['error', 'always'],
-      'no-redeclare': 'error',
-      quotes: ['error', 'single'],
-    },
-  },
+      "no-var": "error",         // interdit var
+      "no-redeclare": "error",   // interdit redéclaration
+      "no-undef": "error",       // signale variables non déclarées
+      "semi": ["error", "always"],   // point-virgule obligatoire
+      "quotes": ["error", "single"]  // guillemets simples
+    }
+  }
 ];
