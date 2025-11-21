d4b4e00476d7ae12bc6d14ac31fbd8faa42d404a
Dodo-31 <botte6230@gmail.com>
2025-11-21 12:19:48 +0100

    3.0.js

---
+++ 3.0.js
@@ -0,0 +1,28 @@
+const prompt = require('prompt-sync')({ sigint: true });
+
+// Tableau contenant tous les apprenants
+let names = [
+  "thelichekinge",
+  "ethan",
+  "Hetaska",
+  "Enrique2802",
+  "Trollman341",
+  "Kisame25021990",
+  "Léandre Mpore-Ghozzo",
+  "Abdoul",
+  "Aucouturier Jason",
+  "xam1712",
+  "Dodo-31",
+  "MattWare",
+  "Thomas",
+  "lalbecade",
+  "Gaetan",
+  "Dimitri Gillain",
+  "TyberCusa",
+  "Johan"
+];
+
+// for of parcourt chaque élément (elem) du tableau (names).
+for (let elem of names) {
+    console.log(elem);
+}
