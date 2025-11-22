0c1f8b183a393a211a700b9e600116d00a7403a0
Dodo-31 <botte6230@gmail.com>
2025-11-22 14:36:39 +0100

    4.1.js

---
+++ 4.1.js
@@ -0,0 +1,15 @@
+const prompt = require("prompt-sync")();
+
+// declaration de ma fonction flechée qui vas calculer la surface de mon rectangle
+
+const calcSurface = (length, width) => length * width;
+
+// Demande a l'utilisateur d'entrer la longueur et la largeur du rectangle
+const length = Number(prompt("Entrez la longueur du rectangle : "));
+const width = Number(prompt("Entrez la largeur du rectangle : "));
+// Appel de la fonction et affichage du résultat
+const surface = calcSurface(length, width);
+
+// Affichage du résultat
+console.log(`La surface du rectangle est : ${surface}`);
+
